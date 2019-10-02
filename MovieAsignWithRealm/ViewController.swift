5908837f9fc9757d6bd4c6ba5d940e2b302603a8
sawpyae <sawpyae.htun@haulio.io>
2019-10-02 12:11:56 +0630

    Initial Commit

---
+++ MovieAsignWithRealm/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MovieAsignWithRealm
+//
+//  Created by saw pyaehtun on 02/10/2019.
+//  Copyright © 2019 saw pyaehtun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
